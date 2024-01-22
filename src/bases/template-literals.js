31b77df03efe02b3c7e89017028658cdca6d354a
matite <matiastite1@gmail.com>
2024-01-22 18:50:56 -0500

    Template literals use example in js

---
+++ src/bases/template-literals.js
@@ -0,0 +1,8 @@
+const name = "Matias";
+const lastName = "Tite";
+
+function getGreeting(name, surname) {
+  return `Hola ${name} ${surname}!`;
+}
+
+console.log(`Ejemplo de template literals: ${getGreeting("Matias", "Tite")}`);
