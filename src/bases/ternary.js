1ffd7382b5c979dca4287974e5a7fd56be9e32bc
matite <matiastite1@gmail.com>
2024-01-26 10:50:51 -0500

    Example of ternary in js

---
+++ src/bases/ternary.js
@@ -0,0 +1,9 @@
+let firstName;
+let lastName;
+
+// firstName != undefined && lastName != undefined
+//   ? console.log(`${firstName} ${lastName}`)
+//   : console.log("Sin nombre o apellido");
+
+const isActive = true;
+const message = isActive ? console.log("Activo") : "No esta activo";
